0a3e69666bbfae136a42a396cc77eee1f5c3fe71
hds <hdszju@163.com>
2019-11-08 17:54:40 +0800

    l5

---
+++ include/l5.h
@@ -0,0 +1,29 @@
+//
+// Created by hds on 11/8/19.
+//
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ */
+#ifndef LEETCODE_L5_H
+#define LEETCODE_L5_H
+
+#include <iostream>
+#include <string>
+#include <map>
+namespace l5 {
+    using namespace std;
+    class Solution {
+    public:
+        string longestPalindrome(string s) {
+
+        }
+    };
+
+    void  test(){
+        Solution s = Solution();
+        string str("adbdsgbsd");
+
+    }
+}
+#endif //LEETCODE_L5_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "include/l3.h"
+#include "include/l5.h"
 using namespace std;
 int main() {
-    cout << l3::test() << endl;
+//    cout << l3::test() << endl;
+    l5::test();
     return 0;
 }
