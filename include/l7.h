ce497a9561d14619ac2d091520e91d773a50d632
hds <hdszju@163.com>
2019-12-25 18:47:27 +0800

    test

---
+++ include/l7.h
@@ -0,0 +1,54 @@
+//
+// Created by hds on 11/12/19.
+//
+/**
+ *将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列
+ */
+
+/**
+ 解题思路
+ 递归法
+
+ * */
+#ifndef LEETCODE_L7_H
+#define LEETCODE_L7_H
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+namespace l7{
+    class Solution {
+    public:
+        string convert(string s, int numRows) {
+            if (numRows==1) return s;            
+
+            vector<string> rows(min(numRows,int(s.size())));
+            int curRow = 0;
+            bool goingDown = false;
+
+            for(char c:s){
+                rows[curRow] += c;   //追加
+
+                if (curRow==0 || curRow==numRows-1) goingDown = !goingDown;  //翻转                
+                curRow += goingDown?1:-1;   
+            }
+
+            //拼接
+            string ret;
+            for(string row:rows) ret += row;
+            return ret;
+        }
+    };
+
+    void test(){
+        Solution s = Solution();
+        
+        string str("LEETCODEISHIRING");
+        cout << s.convert(str,3) << endl;
+
+    }
+
+}
+#endif //LEETCODE_L7_H
--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "include/l4.h"
 #include "include/l5.h"
 #include "include/l6.h"
+#include "include/l7.h"
 #include "include/l468.h"
 int main() {
 //    l4::test();
-    cout << "-----------------------l468----------------------------" << endl;
-    l468::test();
+    cout << "-----------------------l7----------------------------" << endl;
+    l7::test();
 
     int i =0;
     cin >> i;
