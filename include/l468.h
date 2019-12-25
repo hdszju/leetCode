8ad979ba3520ce7feca28a3983027da854b522f6
hds <hdszju@163.com>
2019-12-25 18:36:06 +0800

    test

--- include/l468.h
+++ include/l468.h
@@ -73,7 +73,7 @@ namespace l468{
     void test(){
         Solution s = Solution();
         
-        string str("172.16.254.1");
+        string str("2001:0db8:85a3:0:0:8A2E:0370:7334");
         cout << s.validIPAddress(str) << endl;
 
     }
